ad8cb347fef37e28c168728b40aacd947e698e4f
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-11-06 17:14:27 -0400

    Models Creation

--- models.py
+++ models.py
@@ -1,5 +1,5 @@
 #Here we are gonug to build our database
-from sqlalchemy import create_engine,Column
+from sqlalchemy import create_engine,Column,Integer,String
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 
@@ -16,4 +16,7 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-
+class Person(Base):
+    __tablename__ ='Person'
+    id = Column(Integer, primary_key=True)
+    name = Column(String(50) )
