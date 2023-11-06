2d0ec31ea35a86a1df88cfdabd3836b04b745c5d
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-11-06 13:28:01 -0400

    Models and database_connection

--- main.py
+++ main.py
@@ -1,2 +1,21 @@
 from fastapi import FastAPI
+from sqlalchemy import create_engine,Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+
+app = FastAPI()
+def return_session():    
+
+    USER = 'root'
+    PASSWORD = ''
+    HOST = 'localhost'
+    DB = 'api'
+    PORT = '3306'
+
+    CONN = f'mysql+pymysql;//{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
+
+    engine = create_engine(CONN, echo =True)
+    Session = sessionmaker(bind=engine)
+    return Session
 
--- models.py
+++ models.py
@@ -1,1 +1,19 @@
 #Here we are gonug to build our database
+from sqlalchemy import create_engine,Column
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+USER = 'root'
+PASSWORD = ''
+HOST = 'localhost'
+DB = 'api'
+PORT = '3306'
+
+CONN = f'mysql+pymysql;//{USER}:{PASSWORD}@{HOST}:{PORT}/{DB}'
+
+engine = create_engine(CONN, echo =True)
+Session = sessionmaker(bind=engine)
+session = Session()
+Base = declarative_base()
+
+
