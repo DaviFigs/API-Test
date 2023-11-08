f450c62d39199ab3de73e963a030cd17da10a251
Davi Figueiredo <devdeoliveira06@gmail.com>
2023-11-08 17:31:43 -0400

    Creating Models and RelationsShips

--- main.py
+++ main.py
@@ -1,10 +1,11 @@
 #Here we gonna make requests for server
 from fastapi import FastAPI
+from models import Employe,Supervisor,Manager,Operator
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 from models import CONN
-
+from defs import register_new_person
 app = FastAPI()
 
 def return_session():#This function returns the session function witch we will use to access database
@@ -16,4 +17,25 @@ def return_session():#This function returns the session function witch we will u
 @app.get('/home')
 def home():
     return {'msg':'Welcome to my API '}
+
+@app.post('/register')
+def register_new_employe(option:int,name:str, cpf:str,salary:float, function:str):
+    try:
+        session = return_session()
+        if option == 1:#register supervisor
+            employe = Employe(name = name, cpf = cpf, salary = salary)
+            employe.supervisor = [Supervisor(sector = function)]
+            session.add(employe)
+            session.commit()
+        elif option == 2:
+            pass
+        elif option == 3:
+            pass
+        
+
+        return {'success':'User was created!'}
+    except:
+        return {'error':'erro'}
+
+
 
--- models.py
+++ models.py
@@ -1,7 +1,7 @@
 #Here we are gonug to build our database tables and columns
-from sqlalchemy import create_engine,Column,Integer,String
+from sqlalchemy import create_engine,Column,Integer,String, Float,ForeignKey
 from sqlalchemy.ext.declarative import declarative_base
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, relationship
 
 USER = 'root'
 PASSWORD = 'mysql'
@@ -16,9 +16,50 @@ Session = sessionmaker(bind=engine)
 session = Session()
 Base = declarative_base()
 
-class Person(Base):
-    __tablename__ ='Person'
+class Employe(Base):
+    __tablename__ = 'employe'
     id = Column(Integer, primary_key=True)
-    name = Column(String(50))
-    cpf = Column(String(11))
+    name = Column(String(30), nullable=False)
+    cpf = Column(String(11),nullable=False)
+    salary = Column(Float, nullable=False)
+    supervisor = relationship("Supervisor", back_populates="employe")
+    manager = relationship("Manager", back_populates="employe")
+    operator = relationship("Operator", back_populates="employe")
+
+class Supervisor(Employe):
+    __tablename__ = 'supervisor'
+    id = Column(Integer, primary_key= True)
+    id_employe = Column(Integer, ForeignKey('employe.id'))
+    sector = Column(String(30), nullable=False)
+    employe = relationship("Employe", back_populates="supervisor")
+
+class Manager(Employe):
+    __tablename__ = 'manager'
+    id = Column(Integer, primary_key=True)
+    id_employe = Column(Integer, ForeignKey('employe.id'))
+    unit = Column(String(30), nullable=False)
+    employe = relationship("Employe", back_populates="manager")
+
+
+class Operator(Employe):
+    __tablename__ = 'operator'
+    id = Column(Integer, primary_key=True)
+    id_employe = Column(Integer, ForeignKey('employe.id'))
+    function = Column(String(30), nullable=False)
+    employe = relationship("Employe", back_populates="operator")
+
+
+Base.metadata.create_all(engine)
+
+
+
+
+
+
+
+
+
+
+    
+
 
